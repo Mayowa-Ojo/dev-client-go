ec1baf4d59f6afab46d919f96b5aab91f2d317e2
Mayowa-Ojo <mayowaojo.e@gmail.com>
2021-10-28 16:22:32 -0600

    feat: implement GetPublishedArticlesSorted method

--- articles.go
+++ articles.go
@@ -8,32 +8,51 @@ import (
 )
 
 type Article struct {
-	TypeOf                 string   `json:"type_of"`
-	ID                     int32    `json:"id"`
-	Title                  string   `json:"title"`
-	Description            string   `json:"description"`
-	CoverImage             string   `json:"cover_image,omitempty"`
-	ReadablePublishDate    string   `json:"readable_publish_date"`
-	SocialImage            string   `json:"social_image"`
-	TagList                []string `json:"tag_list"`
-	Tags                   string   `json:"tags"`
-	Slug                   string   `json:"slug"`
-	Path                   string   `json:"path"`
-	URL                    string   `json:"url"`
-	CanonicalURL           string   `json:"canonical_url"`
-	CommentsCount          int32    `json:"comments_count"`
-	PositiveReactionsCount int32    `json:"positive_reactions_count"`
-	PublicReactionsCount   int32    `json:"public_reactions_count"`
-	CreatedAt              string   `json:"created_at"`
-	EditedAt               string   `json:"edited_at,omitempty"`
-	CrosspostedAt          string   `json:"crossposted_at,omitempty"`
-	PublishedAt            string   `json:"published_at"`
-	LastCommentAt          string   `json:"last_comment_at"`
-	PublishedTimestamp     string   `json:"published_timestamp"`
-	User                   *User    `json:"user"`
-	ReadingTimeMinutes     int32    `json:"reading_time_minutes"`
+	TypeOf                 string              `json:"type_of"`
+	ID                     int32               `json:"id"`
+	Title                  string              `json:"title"`
+	Description            string              `json:"description"`
+	CoverImage             string              `json:"cover_image,omitempty"`
+	ReadablePublishDate    string              `json:"readable_publish_date"`
+	SocialImage            string              `json:"social_image"`
+	TagList                []string            `json:"tag_list"`
+	Tags                   string              `json:"tags"`
+	Slug                   string              `json:"slug"`
+	Path                   string              `json:"path"`
+	URL                    string              `json:"url"`
+	CanonicalURL           string              `json:"canonical_url"`
+	CommentsCount          int32               `json:"comments_count"`
+	PositiveReactionsCount int32               `json:"positive_reactions_count"`
+	PublicReactionsCount   int32               `json:"public_reactions_count"`
+	CreatedAt              string              `json:"created_at"`
+	EditedAt               string              `json:"edited_at,omitempty"`
+	CrosspostedAt          string              `json:"crossposted_at,omitempty"`
+	PublishedAt            string              `json:"published_at"`
+	LastCommentAt          string              `json:"last_comment_at"`
+	PublishedTimestamp     string              `json:"published_timestamp"`
+	User                   *User               `json:"user"`
+	ReadingTimeMinutes     int32               `json:"reading_time_minutes"`
+	Organization           *SharedOrganization `json:"organization,omitempty"`
+	FlareTag               *ArticleFlareTag    `json:"flare_tag,omitempty"`
 }
 
+type SharedOrganization struct {
+	Name           string `json:"name"`
+	Username       string `json:"username"`
+	Slug           string `json:"slug"`
+	ProfileImage   string `json:"profile_image"`
+	ProfileImage90 string `json:"profile_image_90"`
+}
+
+type ArticleFlareTag struct {
+	Name         string `json:"name"`
+	BGColorHEX   string `json:"bg_color_hex"`
+	TextColorHEX string `json:"text_color_hex"`
+}
+
+// There are some inconsistencies with regards to the article schema
+// returned when fetching articles and the schema returned when creating
+// articles. This structure fixes that
 type ArticleVariant struct {
 	Article
 	Tags    []string `json:"tags"`
@@ -124,4 +143,28 @@ func (c *Client) CreateArticle(payload ArticleBodySchema, filepath interface{}) 
 	}
 
 	return article, nil
+}
+
+// GetPublishedArticlesSorted allows the client to recieve a list
+// of articles ordered by descending publish date
+func (c *Client) GetPublishedArticlesSorted(q ArticleQueryParams) ([]Article, error) {
+	var articles []Article
+
+	query, err := query.Values(q)
+	if err != nil {
+		return nil, err
+	}
+
+	path := fmt.Sprintf("/articles?%s", query.Encode())
+
+	req, err := c.NewRequest(context.Background(), "GET", path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := c.SendHttpRequest(req, &articles); err != nil {
+		return nil, err
+	}
+
+	return articles, nil
 }
