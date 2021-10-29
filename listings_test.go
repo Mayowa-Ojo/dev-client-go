350db6aed610453aa2519c5d56bb86eccfb73925
Mayowa-Ojo <mayowaojo.e@gmail.com>
2021-10-29 13:24:32 -0600

    test: write GetPublicListings & CreateListing spec

---
+++ listings_test.go
@@ -0,0 +1,51 @@
+package dev
+
+import "testing"
+
+func TestGetPublishedListings(t *testing.T) {
+	c, err := NewTestClient()
+	if err != nil {
+		t.Errorf("Failed to create TestClient: %s", err.Error())
+	}
+
+	listings, err := c.GetPublishedListings(
+		ListingQueryParams{
+			PerPage:  5,
+			Category: "cfp",
+		},
+	)
+
+	if err != nil {
+		t.Errorf("Error fetching articles: %s", err.Error())
+	}
+
+	if listings[0].TypeOf != "listing" {
+		t.Errorf("Expected field 'type_of' to be 'listing', got '%s'", listings[0].TypeOf)
+	}
+
+	if listings[0].Category != "cfp" {
+		t.Errorf("Expected field 'category' to be 'cfp', got '%s'", listings[0].Category)
+	}
+}
+
+func TestCreateListing(t *testing.T) {
+	c, err := NewTestClient()
+	if err != nil {
+		t.Errorf("Failed to create TestClient: %s", err.Error())
+	}
+
+	payload := ListingBodySchema{}
+	payload.Listing.Title = "ACME Conference"
+	payload.Listing.BodyMarkdown = "Awesome conference, come join us!"
+	payload.Listing.Category = "cfp"
+	payload.Listing.Tags = []string{"events"}
+
+	listing, err := c.CreateListing(payload, nil)
+	if err != nil {
+		t.Errorf("Error creating listing: %s", err.Error())
+	}
+
+	if listing.TypeOf != "listing" {
+		t.Errorf("Expected 'type_of' field to be 'listing', got '%s'", listing.TypeOf)
+	}
+}
